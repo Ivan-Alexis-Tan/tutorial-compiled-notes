459d61398d903257f0a0689fafe10eb2b499cf57
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-05-29 00:44:35 +0800

    refactor (route): language routing rewiring

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { Link, Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { initLibRoute, languages, routeData } from "../routeData";
+
+import { languageRoute, languages } from "../routeData";
+import { capsEveryWord } from "../lib/helpers";
+
 import CodingChallengeLanguages from "../coding-challenges/ChallengeLanguages";
 import LanguageCard from "../components/LanguageCard";
 
@@ -13,12 +16,14 @@ export default function Home() {
                 {(languages.length >= 1)
                     ? <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-10">
                         {languages.map(lang => {
-                            const { initUrl } = initLibRoute(lang)
+                            const { url, langTitle } = languageRoute(lang)
+                            const { initUrl } = url
+
                             return (
-                                <Link to={`guides/${lang}/${initUrl}/1`} key={lang}>
-                                    <LanguageCard title={routeData[lang].text} 
+                                <Link to={`/guides/${lang}/${initUrl}/1`} key={lang}>
+                                    <LanguageCard title={langTitle ?? capsEveryWord(lang)} 
                                         language={lang} 
-                                        url={`guides/${lang}/${initUrl}/1`} 
+                                        url={`/guides/${lang}/${initUrl}/1`} 
                                     />
                                 </Link>
                             )
@@ -27,10 +32,6 @@ export default function Home() {
                     : <p>Nothing to display</p>
                 }
             </div>
-
-            {/* <div>
-                <LanguageCard title='python' url={`guides/${lang}/${initUrl}/1`} />
-            </div> */}
 
             <div>
                 <CodingChallengeLanguages />
