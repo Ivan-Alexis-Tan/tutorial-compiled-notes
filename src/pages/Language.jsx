b938c609d0534687d6ff09e0ae5945d22628814b
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-05-29 18:50:19 +0800

    fix (sidebar): routing bug on language selection

--- src/pages/Language.jsx
+++ src/pages/Language.jsx
@@ -11,17 +11,15 @@ export default function LanguagePage() {
     
     if (!data || !librariesData) return <Navigate to={'/not-found'} replace />
 
-    const guideCode = libCode(library)
-
     return (
         <>
             <h2 className='text-2xl font-bold mb-4'>{langTitle} Guides</h2>
                     
             {(libraries.length >= 1)
-                ? <ul className='px-4 text-[16px] max-h-60 overflow-auto'>
-                    {libraries.map(lib => (<li key={lib} className='mb-2'>
-                        <Link to={`/guides/${language}/${lib}/${guideCode}/1`} 
-                            className='inline'
+                ? <ul className='text-[16px] max-h-60 overflow-auto'>
+                    {libraries.map(lib => (<li key={lib} className={`mb-2 px-4`}>
+                        <Link to={`/guides/${language}/${lib}/${libCode(lib)}/1`}
+                            className={`inline`}
                         >
                             {libData(lib).text}
                         </Link>
