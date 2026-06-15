28aa4943d93834a28d5ae58a26a9b6a431cdc83d
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-06-15 13:24:44 +0800

    feat (helpers): added selected guide higlighting function

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -2,3 +2,12 @@ export function capsEveryWord(string, split = " ", inBetween = " ") {
     const caps = `${string}`.split(split).map(s => s.charAt(0).toUpperCase() + s.slice(1))
     return caps.join(inBetween)
 }
+
+export function highlightActive(id, currId) {
+    const baseStyle = `hover:bg-black`
+
+    if (currId === id) {
+        return baseStyle + " bg-black text-(--link-hover-bg-clr)"
+    }
+    return baseStyle
+}
