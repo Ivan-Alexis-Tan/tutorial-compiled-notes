6e77fa1724f93b8ba19c8a15b1062f4200fd2f11
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-07-06 10:40:20 +0800

    perf: improve leveling language selection image

--- src/components/LanguageCard.jsx
+++ src/components/LanguageCard.jsx
@@ -7,14 +7,20 @@ const languageConfig = {
     html: {image: "/html-logo.png"},
     css: {image: "/css-logo.png"},
     git: {image: "/git-logo.png"},
+    sql: {image: "/sql-logo.png"},
 }
 
 export default function LanguageCard({ title = "", language = "", url = "/"}) {
     const imgFile = languageConfig[language]?.image
 
     return (
-        <div className="flex flex-col items-center justify-between p-10 gap-5 hover:border-5 transition-all border rounded-2xl h-66.5">
-            <img src={imgFile} className="w-37.5 h-37.5 object-contain" />
+        <div className="flex flex-col items-center justify-between gap-5 p-10 
+                        hover:border-5 transition-all 
+                        border rounded-2xl h-66.5"
+        >
+            <div className="min-h-37.5">
+                <img src={imgFile} className="w-37.5 h-37.5 object-contain" />
+            </div>            
 
             <h2 className="text-xl font-bold">
                 {capsEveryWord(title)}
