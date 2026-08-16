a145639bf6875ccd414818b08d805abcc9b358b2
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-08-16 16:14:06 +0800

    refactor (guide): change `studentData` into API-like data format

--- src/contents/sql/postgresql/dummyPSQLData.js
+++ src/contents/sql/postgresql/dummyPSQLData.js
@@ -1,17 +1,21 @@
 const today = new Date().toDateString()
 
-export const studentTHeaders = ["id", "name", "marks", "created_at"]
-export const studentsData = {
-    1: [1, "Amir", 23, today],
-    2: [2, "Piyush", 96, today],
-    3: [3, "Riya", 86, today],
-    4: [4, "Ram", 56, today],
-    5: [5, "Shyam", 45, today],
-    6: [6, "Preeti", 67, today],
-    7: [7, "Priya", "[null]", today],
-}
+export const studentsData = [
+    mapStudentData(1, "Amir", 23, today),
+    mapStudentData(2, "Piyush", 96, today),
+    mapStudentData(3, "Riya", 86, today),
+    mapStudentData(4, "Ram", 56, today),
+    mapStudentData(5, "Shyam", 45, today),
+    mapStudentData(6, "Preeti", 67, today),
+    mapStudentData(7, "Priya", "[null]", today),
+]
+export const studentTHeaders = Object.keys(studentsData[0])
 
 export function sliceDummyData(data, start = 0, end) {
     const sliced = Object.entries(data).slice(start, end)
     return Object.fromEntries(sliced)
 }
+
+export function mapStudentData(id, name, marks, createdAt) {
+    return { id, name, marks, created_at: createdAt }
+} 
