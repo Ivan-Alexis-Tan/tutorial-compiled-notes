2479af7ea1708a82d5df35d15aa5848a90b553ce
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-08-19 00:13:40 +0800

    feat (helpers): add `capitalize()` function

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,6 +3,11 @@ export function capsEveryWord(string, split = " ", inBetween = " ") {
     return caps.join(inBetween)
 }
 
+export function capitalize(string, splitBy = " ", joinBy = " ") {
+    const splitted = `${string}`.split(splitBy)
+    return splitted[0].charAt(0).toUpperCase() + splitted.join(joinBy).slice(1)
+}
+
 export function highlightActive(id, currId) {
     const baseStyle = `hover:bg-black`
 
